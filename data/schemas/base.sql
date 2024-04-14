f6d489272f9cd97d01068bbe42261aef60d3d7b0
krippix <krippix@users.noreply.github.com>
2024-04-14 13:42:31 +0200

    changed database structure

--- data/schemas/base.sql
+++ data/schemas/base.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS discord_users(
     user_id BIGINT PRIMARY KEY
 );
 ALTER TABLE discord_users ADD COLUMN IF NOT EXISTS name TEXT;
+ALTER TABLE discord_users ADD COLUMN IF NOT EXISTS uploader BOOLEAN default FALSE;
 
 -- discord_servers
 CREATE TABLE IF NOT EXISTS discord_servers(
@@ -20,7 +21,7 @@ CREATE TABLE IF NOT EXISTS discord_user_displaynames(
     FOREIGN KEY (server_id) REFERENCES discord_servers (server_id),
     PRIMARY KEY (user_id,server_id)
 );
-ALTER TABLE discord_user_displaynames ADD COLUMN IF NOT EXISTS displayname TEXT NOT NULL;
+ALTER TABLE discord_user_displaynames ADD COLUMN IF NOT EXISTS displayname TEXT NOT NULL default 'unknown_user';
 
 -- discord_server_admin_groups
 CREATE TABLE IF NOT EXISTS discord_server_admin_groups(
@@ -28,4 +29,33 @@ CREATE TABLE IF NOT EXISTS discord_server_admin_groups(
     group_name TEXT NOT NULL,
     FOREIGN KEY (server_id) REFERENCES discord_servers (server_id),
     PRIMARY KEY (server_id,group_name)
+);
+
+-- files
+CREATE TABLE IF NOT EXISTS files(
+    file_id BIGINT PRIMARY KEY,
+    file_size BIGINT,
+    server_id BIGINT,
+    uploader_id BIGINT,
+    display_name TEXT NOT NULL,
+    file_name TEXT NOT NULL,
+    hash TEXT NOT NULL,
+    public BOOLEAN,
+    FOREIGN KEY (server_id) REFERENCES discord_servers (server_id),
+    FOREIGN KEY (uploader_id) REFERENCES discord_users (user_id)
+);
+
+-- tags
+CREATE TABLE IF NOT EXISTS tags(
+    tag_id BIGINT PRIMARY KEY,
+    tag_name TEXT NOT NULL
+);
+
+-- files_tags
+CREATE TABLE IF NOT EXISTS files_tags(
+    file_id BIGINT,
+    tag_id BIGINT,
+    FOREIGN KEY (file_id) REFERENCES files,
+    FOREIGN KEY (tag_id) REFERENCES tags,
+    PRIMARY KEY (file_id,tag_id)
 );
