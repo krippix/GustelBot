d8bdd9fef63f8a1140511e46ee61398e9597f7d4
krippix <krippix@users.noreply.github.com>
2024-04-14 13:54:42 +0200

    Added handling of audio files.

--- bot/util/filemgr.py
+++ bot/util/filemgr.py
@@ -1,4 +1,5 @@
 # default
+import hashlib
 import logging
 import pathlib
 import random
@@ -9,7 +10,9 @@ import mutagen
 
 
 def get_files_rec(folder: pathlib.Path) -> list[pathlib.Path]:
-    '''Returns list of tuples, format: (path, filename)'''
+    """
+    Returns list of tuples, format: (path, filename)
+    """
     if folder.is_file():
         logging.error("File instead of folder provided!")
         return []
@@ -23,15 +26,16 @@ def get_files_rec(folder: pathlib.Path) -> list[pathlib.Path]:
     return found_files
 
 
-def get_random_file(folder: pathlib.Path, maxlen: int) -> pathlib.Path:
-    """Returns random file from folder, if maxlen is 0 ignore maxlen
+def get_random_file(folder: pathlib.Path, max_len: int) -> pathlib.Path:
+    """
+    Returns random file from folder, if max_len is 0 ignore max_len
     """
     files = get_files_rec(folder)
     files_clean = []
     for file in files:
         try:
-            soundlen = mutagen.File(file).info.length
-            if maxlen == 0 or soundlen <= maxlen and soundlen != 0:
+            sound_len = mutagen.File(file).info.length
+            if max_len == 0 or sound_len <= max_len and sound_len != 0:
                 files_clean.append(file)
         except Exception:
             logging.warning(f"No length found for {file}")
@@ -59,3 +63,40 @@ def search_files(lst: list[pathlib.Path], keyword: str, threshhold=0.65) -> path
     if len(result_list) == 0:
         return None
     return result_list[0][0]
+
+
+def is_audio_file(file: pathlib.Path) -> bool:
+    """
+    Checks if provided file is an audio file.
+    """
+    if file := mutagen.File(file) is None:
+        return False
+    if file.info.length is not None and file.info.length > 0:
+        return True
+    return False
+
+
+def calculate_md5(file: pathlib.Path) -> str:
+    """
+    Calculates MD5 hash of provided file.
+    """
+    hash_md5 = hashlib.md5()
+    buffer_size = 65536
+    with open(file, 'rb') as f:
+        while True:
+            data = f.read(buffer_size)
+            if not data:
+                break
+            hash_md5.update(data)
+    return hash_md5.hexdigest()
+
+
+def move_to_folder(file: pathlib.Path, folder: pathlib.Path):
+    """
+    moves sound file to sound folder to be used by the bot.
+    """
+    if not file.is_file():
+        raise FileNotFoundError
+    if not folder.is_dir():
+        raise NotADirectoryError
+    file.rename(f'{folder}/{file.name}')
